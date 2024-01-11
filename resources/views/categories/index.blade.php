5cfcbaa9d053c48bab28240ce349d7e907d47fe4
hualli <robertotorfe@gmail.com>
2024-01-11 03:01:40 -0300

    modifico el index de categorías

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -6,72 +6,76 @@
     </x-slot>
 
     <div class='container '>
+
         <div class='row'>
             <div class='col-12'>
-                <a href="{{ route('categories.create') }}" class="btn btn-primary">Agregar</a>
-            </div>
-        </div>
-        <div class='row'>
-            <div class='col-12'>
-            <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                
-                <a href="{{ route('categories.create') }}" class="btn btn-primary">Agregar</a>
-            
+                <div class="py-12">
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <div class="p-6 text-gray-900">
+                                <div class='row'>
+                                    <div class='col-11'>
+                                    </div>
+                                    <div class='col-1'>
+                                        <a href="{{ route('categories.create') }}" class="btn btn-primary">Agregar</a>
+                                    </div>
+                                </div>
 
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Nombre</th>
-                                <th style="width:30px;"></th>
-                                <th style="width:30px;">Acciones</th>
-                                <th style="width:30px;"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        <?php
-                        $i = 1;
-                        ?>
-                        @forelse ($categories as $category)
-                            <tr>
-                                <td>{{$i}}</td>
-                                <td>{{ $category->name }}</td>
+                                <table class="table table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Nombre</th>
+                                        <th style="width:30px;"></th>
+                                        <th style="width:30px;">Acciones</th>
+                                        <th style="width:30px;"></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php
+                                    $i = 1;
+                                    ?>
+                                    @forelse ($categories as $category)
+                                        <tr>
+                                            <td>{{$i}}</td>
+                                            <td>{{ $category->name }}</td>
 
-                                <td style="width:30px;">
-                                    <a href="{{ route('categories.show', $category) }}" class="btn btn-success">Ver</a>
-                                </td>
-                                <td style="width:30px;">
-                                    <a href="{{ route('categories.edit', $category) }}" class="btn btn-warning">Editar</a>
-                                </td>
-                                <td style="width:30px;">
-                                    <form action="{{ route('categories.destroy', $category) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger">Eliminar</button>
-                                    </form>
-                                </td>
-                            </tr>
-                                <?php
-                                $i++;
-                                ?>
-                        @empty
-                            <tr class="bg-red-400 text-white text-center">
-                                <td colspan="3" class="border px-4 py-2">{{ __('No hay categorias para mostrar') }}</td>
-                            </tr>
-                        @endforelse
-                        </tbody>
-                    </table>
+                                            <td style="width:30px;">
+                                                <a href="{{ route('categories.show', $category) }}"
+                                                   class="btn btn-success">Ver</a>
+                                            </td>
+                                            <td style="width:30px;">
+                                                <a href="{{ route('categories.edit', $category) }}"
+                                                   class="btn btn-warning">Editar</a>
+                                            </td>
+                                            <td style="width:30px;">
+                                                <form action="{{ route('categories.destroy', $category) }}"
+                                                      method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">Eliminar</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                            <?php
+                                            $i++;
+                                            ?>
+                                    @empty
+                                        <tr class="bg-red-400 text-white text-center">
+                                            <td colspan="3"
+                                                class="border px-4 py-2">{{ __('No hay categorias para mostrar') }}</td>
+                                        </tr>
+                                    @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
-
-            </div>
-        </div>
-    </div>
             </div>
         </div>
     </div>
 
-    
+
 </x-app-layout>
